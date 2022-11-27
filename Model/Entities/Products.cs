f7cc699457d9018fd5c8a756b745665e6945b67a
Takura <takutadi1@gmail.com>
2022-11-27 17:25:48 +0200

    Models created

---
+++ Model/Entities/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace CornerShop.Model.Entities
+{
+	[Table("Products")]
+	public class Products
+	{
+
+		[Key]
+
+		public int Id { get; set; }
+
+		[Required]
+		public String Name { get; set; }
+
+
+		public Products()
+		{
+		}
+	}
+}
+
