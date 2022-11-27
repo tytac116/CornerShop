e7ac362f6b5b89bbbe8080760442bed0f295e4fc
Takura <takutadi1@gmail.com>
2022-11-27 17:38:53 +0200

    product services added

---
+++ Model/Entities/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace CornerShop.Model.Entities
+{
+    public interface IProductService
+    {
+        void Delete(Product prProduct);
+        List<Product> GetAll();
+        List<Product> GetByName(string prName);
+        List<Product> Save(Product prProduct);
+        List<Product> Update(Product prProduct);
+    }
+}
---
+++ Model/Entities/ProductService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace CornerShop.Model.Entities
+{
+    public class ProductService : IProductService
+    {
+        public AppDataContext _context { get; set; }
+
+        public ProductService(AppDataContext prAppDataContext)
+        {
+            _context = prAppDataContext;
+        }
+
+        public List<Product> GetAll()
+        {
+            return null;
+        }
+
+        public List<Product> GetByName(string prName)
+        {
+            return null;
+        }
+
+        public List<Product> Save(Product prProduct)
+        {
+            return null;
+        }
+
+        public List<Product> Update(Product prProduct)
+        {
+            return null;
+        }
+
+        public void Delete(Product prProduct)
+        {
+
+        }
+
+    }
+}
+
